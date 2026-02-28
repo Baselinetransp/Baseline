29b0e410ee62d886d6a0057315780501d055739e
abba-hash <abba.umarmh@gmail.com>
2026-02-28 18:22:28 +0100

    first commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,23 @@
+import prisma from "@baseline/db";
+import { env } from "@baseline/env/server";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "mysql",
+  }),
+
+  trustedOrigins: [env.CORS_ORIGIN],
+  emailAndPassword: {
+    enabled: true,
+  },
+  advanced: {
+    defaultCookieAttributes: {
+      sameSite: "none",
+      secure: true,
+      httpOnly: true,
+    },
+  },
+  plugins: [],
+});
