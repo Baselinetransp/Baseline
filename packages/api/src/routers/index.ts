ff9958e0bfc2b09d066786334aa2139a9626053f
abba-hash <abba.umarmh@gmail.com>
2026-03-03 04:24:13 +0100

    feat(api): implement users router with profile upserts and job saving

--- packages/api/src/routers/index.ts
+++ packages/api/src/routers/index.ts
@@ -2,6 +2,7 @@ import { protectedProcedure, publicProcedure, router } from "../index";
 import { todoRouter } from "./todo";
 import { applicationsRouter } from "./application";
 import { jobsRouter } from "./job";
+import { usersRouter } from "./user";
 
 
 export const appRouter = router({
@@ -16,7 +17,8 @@ export const appRouter = router({
   }),
   todo: todoRouter,
   applications: applicationsRouter,
-  job: jobsRouter
+  job: jobsRouter,
+  users: usersRouter,
 
 });
 export type AppRouter = typeof appRouter;
